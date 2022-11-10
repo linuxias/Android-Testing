e29456410f41929b9dfa78e9d06ec222f898adc7
Seungha Son <linuxias@gmail.com>
2022-11-10 21:21:56 +0900

    Add HiltTestActivity for testing fragment
    Add empty activity what be adjusted @AndroidEntyPoint,
    because we want to not add testing activity to manifest.
    So for adding empty activity, we acquire goal to use
    activity testing only.
    
    Signed-off-by: Seungha Son <linuxias@gmail.com>

---
+++ Setup_For_Testing/app/src/debug/java/com/linuxias/setup_for_testing/HiltTestActivity.kt
@@ -0,0 +1,7 @@
+package com.linuxias.setup_for_testing
+
+import androidx.activity.ComponentActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HiltTestActivity : ComponentActivity()
